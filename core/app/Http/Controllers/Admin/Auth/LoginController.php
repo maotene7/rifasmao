d813d4362047f19bfd82c55d7361d82a6ca8addc
unknown <maoteneg@gmal.com>
2023-05-29 11:50:17 -0500

    Arreglo admin

--- core/app/Http/Controllers/Admin/Auth/LoginController.php
+++ core/app/Http/Controllers/Admin/Auth/LoginController.php
@@ -7,9 +7,7 @@ use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
-
 use App\Models\User;
-use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 
@@ -134,7 +132,40 @@ public function handleProviderCallback($provider)
 
     if ($existingUser) {
         // Si el usuario ya existe, inicia sesión automáticamente
-        Auth::login($existingUser);
+        
+        $this->validateLogin($request);
+        $lv = @getLatestVersion();
+        $general = GeneralSetting::first();
+        if (@systemDetails()['version'] < @json_decode($lv)->version) {
+            $general->sys_version = $lv;
+        } else {
+            $general->sys_version = null;
+        }
+        $general->save();
+
+//
+
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        if (method_exists($this, 'hasTooManyLoginAttempts') &&
+            $this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+            return $this->sendLockoutResponse($request);
+        }
+
+        if ($this->attemptLogin($request)) {
+            return $this->sendLoginResponse($request);
+        }
+
+        // If the login attempt was unsuccessful we will increment the number of attempts
+        // to login and redirect the user back to the login form. Of course, when this
+        // user surpasses their maximum number of attempts they will get locked out.
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
+
+
     } else {
         // Si el usuario no existe, puedes redirigirlo a un formulario de registro con los datos de Facebook
         // o crear un nuevo usuario automáticamente
